158f9fce8301f92ae298c42cf33d7d3dd0bba455
RomanChudov <rc3670h@gmail.com>
2024-10-15 21:15:31 +0000

    venjuleg föll skráð

--- scripts.js
+++ scripts.js
@@ -9,10 +9,10 @@
 // Fastar
 
 /** Íslenskir sérhljóðar */
-const CONSONANTS = 'bcdfghjklmnpqrstvwxz'.split('');
+const CONSONANTS = "bcdfghjklmnpqrstvwxz".split("");
 
 /** Íslenskir samhljóðar */
-const VOWELS = 'aeiouyáéýúíóöæ'.split('');
+const VOWELS = "aeiouyáéýúíóöæ".split("");
 
 //------------------------------------------------------------------------------
 // Hjálparföll
@@ -23,12 +23,12 @@ const VOWELS = 'aeiouyáéýúíóöæ'.split('');
  * @returns `true` ef `str` er strengur, annars `false`.
  */
 // Skilgreinum anonymous fall og bindum við breytuna `isString`
-const isString = (str) => typeof str === 'string';
+const isString = (str) => typeof str === "string";
 
 // Prófum fallið
-console.assert(isString('hi') === true, 'isString: skilar `true` fyrir streng');
-console.assert(isString(42) === false, 'isString: skilar `false` fyrir tölu');
-console.assert(isString(null) === false, 'isString: skilar `false` fyrir null');
+console.assert(isString("hi") === true, "isString: skilar `true` fyrir streng");
+console.assert(isString(42) === false, "isString: skilar `false` fyrir tölu");
+console.assert(isString(null) === false, "isString: skilar `false` fyrir null");
 
 /**
  * Öruggt fall sem skilar fylki af strengjum úr gefnum streng, skipt upp með
@@ -37,7 +37,7 @@ console.assert(isString(null) === false, 'isString: skilar `false` fyrir null');
  * @returns {string[]} Fylki af strengjum eða tóma fylkið ef afmarkari kom
  * ekki fram.
  */
-function split(str, separator = ' ') {
+function split(str, separator = " ") {
   if (!isString(str)) {
     return [];
   }
@@ -49,28 +49,112 @@ function split(str, separator = ' ') {
 // Grunnföll sem skilgreina á
 
 function longest(str) {
-  // Útfæra
+  if (isString(str)) {
+    const stru = split(str);
+    i = 0;
+    for (x in stru) {
+      if (stru[x].length > stru[i].length) {
+        i = x;
+      }
+    }
+    return stru[i];
+  } else {
+    return null;
+  }
 }
+
+console.assert(longest("halló hæ") === "halló", "5 er stærri en 2");
+console.assert(longest("halló hæ bææææ") === "halló", "fysrtur kemur first");
+console.assert(longest("") === "", "tóm gefur tóm");
+console.assert(longest(true) === null, "ekki streng gefur null");
 
 function shortest(str) {
-  // Útfæra
+  if (isString(str)) {
+    const stru = split(str);
+    i = 0;
+    for (x in stru) {
+      if (stru[x].length < stru[i].length) {
+        i = x;
+      }
+    }
+    return stru[i];
+  } else {
+    return null;
+  }
 }
+console.assert(shortest("halló hæ") === "hæ", "2 er minna en 5");
+console.assert(shortest("halló hæ bææææ ha") === "hæ", "fysrtur kemur fyrstur");
+console.assert(shortest("") === "", "tóm gefur tóm");
+console.assert(shortest(true) === null, "ekki streng gefur null");
 
 function reverse(str) {
-  // Útfæra
+  if (isString(str)) {
+    const split = str.split("");
+    const reversed = split.reverse();
+
+    return reversed.join("");
+  }
+  return null;
 }
+console.assert(
+  reverse("halló") === "óllah",
+  "reverse: snýr við einföldum streng"
+);
+console.assert(
+  reverse(false) === null,
+  "reverse: ef ekki strengur, skila null"
+);
 
 function palindrome(str) {
-  // Útfæra
+  if (isString(str) && str !== "") {
+    const reversed = reverse(str);
+    return str.toLowerCase() === reversed.toLowerCase();
+  } else {
+    return false;
+  }
 }
+console.assert(palindrome("halló") === false, "ekki samhverf");
+console.assert(palindrome("Hæh") === true, "samhverf");
+console.assert(palindrome("") === false, "tóm ekki samhverf");
+console.assert(palindrome(29) === false, "ekki streng false");
 
 function vowels(str) {
-  // Útfæra
+  if (isString(str)) {
+    const streng = str.split("");
+    i = 0;
+    for (x in streng) {
+      for (y in VOWELS) {
+        if (streng[x].toLowerCase() === VOWELS[y].toLowerCase()) {
+          i = i + 1;
+        }
+      }
+    }
+    return i;
+  } else {
+    return 0;
+  }
 }
+console.assert(vowels("halló") === 2, "2 sérhljóða");
+console.assert(vowels(false) === 0, "ekki streng");
 
 function consonants(str) {
-  // Útfæra
+  if (isString(str)) {
+    const streng = str.split("");
+    i = 0;
+    for (x in streng) {
+      for (y in CONSONANTS) {
+        if (streng[x].toLowerCase() === CONSONANTS[y].toLowerCase()) {
+          i = i + 1;
+        }
+      }
+    }
+    return i;
+  } else {
+    return 0;
+  }
 }
+console.assert(consonants("halló") === 3, "3 samhljóða");
+console.assert(consonants(false) === 0, "ekki streng");
 
 //------------------------------------------------------------------------------
 // Leiðbeint ferli
